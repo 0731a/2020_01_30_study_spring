7fd0a678c9568da62d4ced17433cfa5b45bea54f
0731a <0731SeoYeong@gmail.com>
2021-02-03 23:18:18 +0900

    [ADD] 어드바이스 동작 시점에 따른 어노테이션 정리

--- src/main/java/com/springbook/biz/common/AOP/LogAdviceByAnnotation1.java
+++ src/main/java/com/springbook/biz/common/AOP/LogAdviceByAnnotation1.java
@@ -14,8 +14,16 @@ public class LogAdviceByAnnotation1 { // Aspect가 설정된 aspect 객체에는 반드시 
 	// 				   스프링 에서는 런타임 위빙 방식 지원
 	// -> 어드바이스 클래스 마다 포인트 컷 설정 필요, 포인트 컷의 재활용 불가 
 	
+	
 	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))") //포인트 컷 메소드 
 	public void allPointcut() {} // 포인트 컷 식별을 위한 참조 메소드 
+	
+	// 어드바이스 동작 시점과 관련된 어노테이션 
+	// @Before: 비즈니스 메소드 실행 전 동작
+	// @AfterReturning: 비즈니스 메소드가 성공적으로 리턴시 동작
+	// @AfterThrowing: 비즈니스 실행 중 예외 발생 시 동작
+	// @After: 비즈니스 메소드 실행 후 무조건 실행 ( 예외 발생 시에도 )
+	// @Around: 메소드 호출 자체를 가로 채 비즈니스 실행 전후에 처리할 로직 삽입 가능 
 	
 	@Before("allPointcut()") // 어드바이스 메소드 
 	public void printLog() {
