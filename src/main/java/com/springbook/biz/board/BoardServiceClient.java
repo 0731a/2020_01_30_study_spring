84eb106cb6997187f3b3fdf105433bd329a44fa2
0731a <0731SeoYeong@gmail.com>
2021-02-07 21:45:38 +0900

    [ADD] login.jsp 추가

--- src/main/java/com/springbook/biz/board/BoardServiceClient.java
+++ src/main/java/com/springbook/biz/board/BoardServiceClient.java
@@ -20,6 +20,12 @@ public class BoardServiceClient {
 		vo.setContent("임시 내용..............");
 		boardService.insertBoard(vo);
 		
+		BoardVO vo2 = new BoardVO();
+		vo2.setTitle("임시 제목4");
+		vo2.setWriter("dmdkdk");
+		vo2.setContent("임시 내용..............");
+		boardService.insertBoard(vo2);
+		
 		// 4. 글 목록 검색 기능 테스트
 		List<BoardVO> boardList = boardService.getBoardList(vo);
 		for(BoardVO board : boardList ) {
