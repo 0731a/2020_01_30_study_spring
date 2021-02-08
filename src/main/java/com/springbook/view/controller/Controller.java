d454416f311786c9fb0e00cbf152d5114053efed
0731a <0731SeoYeong@gmail.com>
2021-02-08 21:58:53 +0900

    [ADD] controller interface

---
+++ src/main/java/com/springbook/view/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.springbook.view.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 어떤 컨트롤러 객체가 검색되더라도 같은 코드로 실행하기 위한 컨트롤러 인터페이스 
+public interface Controller {
+	String handleRequest(HttpServletRequest request, HttpServletResponse response);
+}
