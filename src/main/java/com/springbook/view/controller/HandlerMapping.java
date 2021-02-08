b2ce0fde9b94f11f6805458133d2b6bf9ba71de7
0731a <0731SeoYeong@gmail.com>
2021-02-08 22:11:02 +0900

    [ADD] HandlerMapping

---
+++ src/main/java/com/springbook/view/controller/HandlerMapping.java
@@ -0,0 +1,23 @@
+package com.springbook.view.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.springbook.view.user.LoginController;
+
+//  모든  Controller 객체들을 저장
+// 클라이언트의 요청이 들어 올 경우 요청을 처리할 특정 Controller 검색기능 제공 
+// DispatchServlet이 사용하는 객체  (DispatchServlet init 메소드 호출시 생성 됨)
+public class HandlerMapping {
+	private Map<String, CustomController> mappings;
+	
+	public HandlerMapping() {
+		mappings = new HashMap<String, CustomController>();
+		mappings.put("/login.do",new LoginController());
+	}
+	
+	public CustomController getController(String path) {
+		return mappings.get(path);
+	}
+
+}
