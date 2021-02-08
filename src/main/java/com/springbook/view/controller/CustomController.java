412c1708ac6f2464b535be54e35a5f69190d0cc9
0731a <0731SeoYeong@gmail.com>
2021-02-08 22:05:10 +0900

    [ADD] CustomController을 확장한 LoginController 추가

--- src/main/java/com/springbook/view/controller/CustomController.java
+++ src/main/java/com/springbook/view/controller/CustomController.java
@@ -4,6 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // 어떤 컨트롤러 객체가 검색되더라도 같은 코드로 실행하기 위한 컨트롤러 인터페이스 
-public interface Controller {
+public interface CustomController {
 	String handleRequest(HttpServletRequest request, HttpServletResponse response);
 }
