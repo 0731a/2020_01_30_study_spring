4a72ca2e840c70837e4d1f97f7edddd137dea59a
0731a <0731SeoYeong@gmail.com>
2021-02-08 21:55:39 +0900

    [ADD] Dispatch Controller을 통한 getBoardList 처리

--- src/main/java/com/springbook/view/controller/CustomDispatchController.java
+++ src/main/java/com/springbook/view/controller/CustomDispatchController.java
@@ -2,11 +2,16 @@ package com.springbook.view.controller;
 
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAOByJDBC;
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
@@ -62,7 +67,7 @@ public class CustomDispatchController extends HttpServlet {
 			
 			// 3. 화면 네비게이션
 			if( user != null ){
-				response.sendRedirect("getBoardList.jsp");
+				response.sendRedirect("getBoardList.do");
 			}else{
 				response.sendRedirect("login.jsp");
 			}
@@ -78,6 +83,26 @@ public class CustomDispatchController extends HttpServlet {
 			System.out.println("글 상세 조회 처리");
 		}else if(path.equals("/getBoardList.do")) {
 			System.out.println("글 목록 검색 처리");
+			
+			// 1. 사용자 입력 정보 추출(검색 기능은 나중에 구현)
+			// 2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			BoardDAOByJDBC boardDAO = new BoardDAOByJDBC();
+			List<BoardVO> boardList = boardDAO.getBoardList(vo);
+			
+			
+			/* 세션은 브라우저당 서버 메모리에 하나씩 유지되는 객체이므로 서버 입장에서 부담스러움
+			 * HttpServletRequest를 이용하여 서버에 부담 줄이기
+			 * HttpServletRequest 객체는 클라이언트가 서버에 요청을 전송할때마다 매번 새롭게 생성, 응답 반환시 바로 삭제되는 1회성 객체 
+			// 3. 검색 결과를 세션에 저장하고 목록 화면으로 이동한다.
+			HttpSession session = request.getSession();
+			session.setAttribute("boardList", boardList);
+			response.sendRedirect("getBoardList.jsp");
+			*/
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("boardList", boardList);
+			response.sendRedirect("getBoardListModel2.jsp");
 		}
 	}
 
